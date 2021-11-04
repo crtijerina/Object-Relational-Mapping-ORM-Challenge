cca0ea396afb572afcb4d70140bb46bc9ba979df
Chris Tijerina <crtijerina1988@gmail.com>
2021-11-04 18:01:52 -0500

    this is it finally

--- Main/routes/api/index.js
+++ Main/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
-
+//11/4/2021 have accsess to them
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
